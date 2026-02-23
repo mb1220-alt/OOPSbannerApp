10a6ce1fb5bf232e03b01a4d94f3108f7027253c
mohith <mb1220@srmist.edu.in>
2026-02-23 22:44:37 +0530

    Completed UC4: String Array and Loop implementation

--- UC4/OOPSBannerApp.java
+++ UC4/OOPSBannerApp.java
@@ -1,26 +1,29 @@
 /**
- * OOPSBannerApp
- * Renders OOPS banner using String.join()
- * @author YourName
- * @version 3.0
+ * UC4: Render OOPS as Banner using String Array and Loop
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ",
-                "*******", "*******", "****** ", "*******"));
-        System.out.println(String.join(" ",
-                "*     *", "*     *", "*     *", "*      "));
-        System.out.println(String.join(" ",
-                "*     *", "*     *", "*     *", "*      "));
-        System.out.println(String.join(" ",
-                "*     *", "*     *", "****** ", "****** "));
-        System.out.println(String.join(" ",
-                "*     *", "*     *", "*      ", "      *"));
-        System.out.println(String.join(" ",
-                "*     *", "*     *", "*      ", "*     *"));
-        System.out.println(String.join(" ",
-                "*******", "*******", "*      ", "*******"));
+        String[] banner = new String[7];
+
+        banner[0] = String.join(" ",
+                "*******", "*******", "****** ", "*******");
+        banner[1] = String.join(" ",
+                "*     *", "*     *", "*     *", "*      ");
+        banner[2] = String.join(" ",
+                "*     *", "*     *", "*     *", "*      ");
+        banner[3] = String.join(" ",
+                "*     *", "*     *", "****** ", "****** ");
+        banner[4] = String.join(" ",
+                "*     *", "*     *", "*      ", "      *");
+        banner[5] = String.join(" ",
+                "*     *", "*     *", "*      ", "*     *");
+        banner[6] = String.join(" ",
+                "*******", "*******", "*      ", "*******");
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
