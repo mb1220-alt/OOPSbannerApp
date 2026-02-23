4ad5ae5937230115b1a6602643dc0b6de787bafe
mohith <mb1220@srmist.edu.in>
2026-02-23 22:34:31 +0530

    Recreated UC2

---
+++ UC2/OOPSBannerApp.java
@@ -0,0 +1,16 @@
+/**
+ * UC2: Render OOPS as Banner using String Concatenation
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("*******" + " " + "*******" + " " + "****** " + " " + "*******");
+        System.out.println("*     *" + " " + "*     *" + " " + "*     *" + " " + "*      ");
+        System.out.println("*     *" + " " + "*     *" + " " + "*     *" + " " + "*      ");
+        System.out.println("*     *" + " " + "*     *" + " " + "****** " + " " + "****** ");
+        System.out.println("*     *" + " " + "*     *" + " " + "*      " + " " + "      *");
+        System.out.println("*     *" + " " + "*     *" + " " + "*      " + " " + "*     *");
+        System.out.println("*******" + " " + "*******" + " " + "*      " + " " + "*******");
+    }
+}
