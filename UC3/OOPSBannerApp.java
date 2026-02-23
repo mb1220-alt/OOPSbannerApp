ebed7ae12aced0102e533ac0b0d194b1adad78d9
mohith <mb1220@srmist.edu.in>
2026-02-23 22:30:16 +0530

    Recovered correct UC3 version

---
+++ UC3/OOPSBannerApp.java
@@ -0,0 +1,26 @@
+/**
+ * OOPSBannerApp
+ * Renders OOPS banner using String.join()
+ * @author YourName
+ * @version 3.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                "*******", "*******", "****** ", "*******"));
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*     *", "*      "));
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*     *", "*      "));
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "****** ", "****** "));
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*      ", "      *"));
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*      ", "*     *"));
+        System.out.println(String.join(" ",
+                "*******", "*******", "*      ", "*******"));
+    }
+}
